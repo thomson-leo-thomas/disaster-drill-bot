bea80c4f64e3f3b121f093d6377b9be3cba3c472
thomson-leo-thomas <thomsonleothomas2004@gmail.com>
2025-06-23 20:09:16 +0600

    port 8080 ftom 5000

--- app.py
+++ app.py
@@ -300,5 +300,5 @@ def global_error_handler(e):
     return {"error": "Something went wrong."}, 500
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 8080))
     app.run(debug=False, host="0.0.0.0", port=port)
