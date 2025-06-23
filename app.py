314d725a0a65209f1ebf9569850a48dc748cece1
thomson-leo-thomas <thomsonleothomas2004@gmail.com>
2025-06-23 20:16:52 +0600

    still testing explict procfile command

--- app.py
+++ app.py
@@ -301,4 +301,5 @@ def global_error_handler(e):
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8080))
+    print(f"🧠 Sensei is thinking... Flask is starting on port {port}.")
     app.run(debug=False, host="0.0.0.0", port=port)
